bec2fa8945368040b2f0efe5857841a4c8eb39ef
Shao <shao@abv.bg>
2016-04-20 22:31:03 +0300

    Update and rename CPU.java to Buttons.java

---
+++ src/Game/Buttons.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by ALEKSANDAR on 4/19/2016.
+ */
+public class Buttons {
+
+    public static boolean doRandomMove(JButton jButton) {
+        return false;
+    }
+
+    public static int doMove(JButton jButton0, JButton jButton1, JButton jButton2, JButton jButton3,
+                             JButton jButton4, JButton jButton5, JButton jButton6, JButton jButton7,
+                             JButton jButton8, JButton jButton9, JButton jButton10, JButton jButton11,
+                             JButton jButton12, JButton jButton13, JButton jButton14, JButton jButton15) {
+        return 0;
+    }
+}
