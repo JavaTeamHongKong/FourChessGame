388fb3694b42ee83047927462ec9efc02fe27a3e
Yordan Hristov <yordan.h@abv.bg>
2016-04-20 20:23:46 +0300

    CPU-Class

--- src/Game/Fourchess.java
+++ src/Game/Fourchess.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import Game.CPU;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FourChess implements ActionListener {
     //Setting up ALL the variables
